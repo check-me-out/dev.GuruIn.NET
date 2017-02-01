d60a1d5811a5cc16018f7212ee75ddb93d56c70d
Shree Venkat <Shree Venkat>
2017-02-01 20:16:25 +0000

    Initial draft

---
+++ Sample.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,91 @@
+﻿using System.Web.Optimization;
+
+namespace Sample.Web
+{
+    public class BundleConfig
+    {
+        #region bundles
+
+        private static readonly string[] Jquery =       {
+                                                            "~/Scripts/jquery/jquery-{version}.js"
+                                                        };
+
+        private static readonly string[] JqueryVal =    {
+                                                            "~/Scripts/jquery/jquery.validate*"
+                                                        };
+
+        private static readonly string[] Bootstrap =    {
+                                                            "~/Scripts/bootstrap/bootstrap.js"
+                                                        };
+
+        private static readonly string[] Angular =      {
+                                                            //"~/Scripts/angular/angular.js",
+                                                            "~/Scripts/angular/angular-animate.js",
+                                                            "~/Scripts/angular/angular-filter.js",
+                                                            "~/Scripts/angular/angular-loader.js",
+                                                            "~/Scripts/angular/angular-local-storage.js",
+                                                            "~/Scripts/angular/angular-messages.js",
+                                                            "~/Scripts/angular/angular-mocks.js",
+                                                            "~/Scripts/angular/angular-resource.js",
+                                                            "~/Scripts/angular/angular-route.js",
+                                                            "~/Scripts/loading-bar.js"
+                                                        };
+
+        private static readonly string[] UiBootstrap =  {
+                                                            "~/Scripts/bootstrap/ui/ui-bootstrap.js"
+                                                        };
+
+        private static readonly string[] UiRouter =     {
+                                                            "~/Scripts/angular/ui/angular-ui-router.js",
+                                                            "~/Scripts/angular/ui/angular-ui-router-tabs.js"
+                                                        };
+
+        private static readonly string[] UiSelect =     {
+                                                            "~/Scripts/angular/ui/angular-ui-select.js"
+                                                        };
+
+        private static readonly string[] Misc =         {
+                                                            //"~/Scripts/misc/modernizr-*",
+                                                            "~/Scripts/misc/respond.js",
+                                                            "~/Scripts/misc/loading-bar.js",
+                                                            "~/Scripts/misc/toaster.js"
+                                                        };
+
+        #endregion
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+            bundles.Add(new StyleBundle("~/Content/css/deferred-css").Include(
+                        "~/Content/css/loading-bar.css",
+                        "~/Content/css/toaster.css",
+                        "~/Content/css/ui-select.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(Jquery));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(JqueryVal));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(Bootstrap));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(Angular));
+
+            bundles.Add(new ScriptBundle("~/bundles/ui-bootstrap").Include(UiBootstrap));
+
+            bundles.Add(new ScriptBundle("~/bundles/ui-router").Include(UiRouter));
+
+            bundles.Add(new ScriptBundle("~/bundles/ui-select").Include(UiSelect));
+
+            bundles.Add(new ScriptBundle("~/bundles/toaster").Include(Misc));
+
+            bundles.Add(new ScriptBundle("~/bundles/third-party-libs")
+                                                        //.Include(Jquery)
+                                                        //.Include(Bootstrap)
+                                                        .Include(Angular)
+                                                        .Include(UiBootstrap)
+                                                        .Include(UiRouter)
+                                                        .Include(UiSelect)
+                                                        .Include(Misc));
+        }
+    }
+}
---
+++ Sample.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Sample.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Version()
+        {
+            var ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            var builtOn = new DateTime(2000, 1, 1);
+            builtOn = builtOn.AddDays(ver.Build);
+            var buildNumber = (DateTime.Now.Year - 2000) + builtOn.DayOfYear.ToString("#000");
+            builtOn = builtOn.AddSeconds(ver.Revision * 2);
+
+            return Content(string.Format("<div style=\"font-family: Verdana; font-size: 13px\">DLL version: {0}<br /> Build number: {1} (built on {2})</div>", ver, buildNumber, builtOn), "text/html");
+        }
+    }
+}
